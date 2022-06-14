f63c98353c139b3e6215c75caa8cfe26a2dffa84
tomey62 <tomek>
2022-06-14 20:53:20 +0200

    updated login credentials

--- src/main/java/pl/zukowski/jwtauth/controller/LoginController.java
+++ src/main/java/pl/zukowski/jwtauth/controller/LoginController.java
@@ -1,11 +1,10 @@
 package pl.zukowski.jwtauth.controller;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import pl.zukowski.jwtauth.dto.UserDto;
+import pl.zukowski.jwtauth.dto.LoginCredentials;
 
 import javax.validation.Valid;
 
@@ -14,7 +13,7 @@ import javax.validation.Valid;
 public class LoginController {
 
     @PostMapping("/login")
-    public void authenticate(@Valid @RequestBody UserDto dto)
+    public void authenticate(@Valid @RequestBody LoginCredentials loginCredentials)
     {
 
     }
--- src/main/java/pl/zukowski/jwtauth/filter/AuthenticationFilter.java
+++ src/main/java/pl/zukowski/jwtauth/filter/AuthenticationFilter.java
@@ -4,7 +4,6 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
-import org.modelmapper.ModelMapper;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -12,7 +11,7 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import pl.zukowski.jwtauth.dto.UserDto;
+import pl.zukowski.jwtauth.dto.LoginCredentials;
 import javax.servlet.FilterChain;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -42,10 +41,10 @@ public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
             {
                 builder.append(line);
             }
-            UserDto dto = objectMapper.readValue(builder.toString(),UserDto.class);
+            LoginCredentials login = objectMapper.readValue(builder.toString(),LoginCredentials.class);
         /* String login = request.getParameter("login");
         String password = request.getParameter("password");*/
-            UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(dto.getLogin(), dto.getPassword());
+            UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(login.getLogin(), login.getPassword());
             return authenticationManager.authenticate(authenticationToken);
         } catch (IOException e) {
             throw new IllegalArgumentException(e.getMessage());
