e7ba2fd2914acf7c52fdb0b000f51bfacc80c175
tomey62 <tomek>
2022-06-15 11:51:52 +0200

    added cors to security

--- src/main/java/pl/zukowski/jwtauth/JwtauthApplication.java
+++ src/main/java/pl/zukowski/jwtauth/JwtauthApplication.java
@@ -1,19 +1,13 @@
 package pl.zukowski.jwtauth;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import pl.zukowski.jwtauth.entity.Role;
-import pl.zukowski.jwtauth.entity.User;
-import pl.zukowski.jwtauth.service.UserService;
 
-import java.util.ArrayList;
 
 @SpringBootApplication
 public class JwtauthApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JwtauthApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(JwtauthApplication.class, args);
+    }
 }
