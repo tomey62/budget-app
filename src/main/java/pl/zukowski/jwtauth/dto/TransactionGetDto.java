b84a7c87872358702420132e045953f182014433
tomey62 <tomek>
2022-06-21 13:15:05 +0200

    Added noargsconstructor to dto

--- src/main/java/pl/zukowski/jwtauth/dto/TransactionGetDto.java
+++ src/main/java/pl/zukowski/jwtauth/dto/TransactionGetDto.java
@@ -3,9 +3,11 @@ package pl.zukowski.jwtauth.dto;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class TransactionGetDto {
     private Long price;
     private String category;
